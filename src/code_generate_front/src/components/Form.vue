f1990cd4cbfbda187657c765dceceb5ef7c80476
ycp19940225 <820363773@qq.com>
2022-05-05 18:22:10 +0800

    add 前台

--- src/code_generate_front/src/components/Form.vue
+++ src/code_generate_front/src/components/Form.vue
@@ -16,9 +16,8 @@
             <el-input v-model="form.title" />
         </el-form-item>
         <el-form-item label="sql">
-            <el-input v-model="form.sql" type="textarea" />
+            <el-input v-model="form.sql" type="textarea" @blur="createInputElement()"/>
         </el-form-item>
-
         <el-form-item
                 v-for="(info, index) in form.formInfo"
                 :key="index"
@@ -26,12 +25,35 @@
                 :prop="info.name"
                 :rules="info.rules"
         >
-            <el-input v-model="info.value" />
+            <el-form :inline="true" class="">
+                <el-form-item label="字段名">
+                    <el-input v-model="info.value" placeholder="请输入字段名" />
+                </el-form-item>
+                <el-form-item label="字段值">
+                    <el-input v-model="info.value" placeholder="请输入字段值" />
+                </el-form-item>
+                <el-form-item label="字段类型">
+                    <el-select v-model="info.value" placeholder="请选择字段类型">
+                        <el-option label="文本" value="input" />
+                        <el-option label="选择" value="select" />
+                        <el-option label="日期" value="date" />
+                        <el-option label="单选" value="radio" />
+                        <el-option label="复选" value="checkbox" />
+                        <el-option label="文本" value="textarea" />
+                        <el-option label="文件" value="file" />
+                        <el-option label="富文本" value="rich_textarea" />
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="是否为空">
+                    <el-select v-model="info.value" placeholder="是否为空">
+                        <el-option label="文本" value="input" />
+                        <el-option label="文本" value="input" />
+                    </el-select>
+                </el-form-item>
+            </el-form>
         </el-form-item>
-
         <el-form-item>
             <el-button type="primary" @click="onSubmit">Create</el-button>
-            <el-button>Cancel</el-button>
         </el-form-item>
     </el-form>
 </template>
@@ -53,8 +75,18 @@
             name:'test',
             rules:{
                
+            },
+        },
+            {
+                label:'test',
+                prop:'ss',
+                name:'test',
+                rules:{
+
+                },
+
             }
-        }],
+        ],
     })
 
     const onSubmit = () => {
@@ -68,5 +100,27 @@
                 })
             }
         })
+    }
+
+    function createInputElement(){
+        let data = form.sql;
+        data = data.toString().split(',');
+        data = formatString(data);
+        console.log(data)
+        let inputElement = [];
+        for(let i = 0; i < data.length; i++){
+            inputElement.push([
+
+            ])
+        }
+    }
+
+    function formatString(str){
+        for(let i = 0; i < str.length; i++){
+            let temp = str[i];
+            temp = temp.replace('/\n/g', '')
+            str[i] = temp.trim()
+        }
+        return str;
     }
 </script>
